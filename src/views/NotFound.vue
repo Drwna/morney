8d7c5e9f1a489110dd4cd214d2198adc12ef5011
draganet <3417237276@qq.com>
2021-11-09 22:21:20 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      <div>404</div>
+      <div>
+        <router-link to="/">返回首页</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
