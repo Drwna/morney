991b53a61c25771c43f6473373c5f276f01c7a0e
draganet <3417237276@qq.com>
2021-11-09 23:19:33 +0800

    Labels Statistics 页面布局

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    Statistics.vue
+
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Statistics.vue</p>
+
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,6 +16,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.nav-wrapper {
+  border: 2px solid blue;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  border: 1px solid purple;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
