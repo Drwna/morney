075a08a7602edd7c4223aa2b9591f784c57f3ee2
draganet <3417237276@qq.com>
2021-11-10 20:18:07 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,17 @@
 <template>
   <nav class="nav">
 
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>
       标签
     </router-link>
 
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
 
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -20,14 +20,6 @@
 </template>
 
 <script lang="ts">
-// import x from '@/assets/icons/labels.svg';
-// import y from '@/assets/icons/money.svg';
-// import z from '@/assets/icons/statistics.svg';
-//
-// console.log(x);
-// console.log(y);
-// console.log(z);
-
 
 export default {
   name: 'Nav',
@@ -35,6 +27,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
@@ -53,6 +46,10 @@ nav {
       width: 32px;
       height: 32px;
     }
+  }
+
+  > .item.selected {
+    color: $color-hightlight;
   }
 }
 
