53e2614d1a666f358b665a6d623f4110e1f5a70c
draganet <3417237276@qq.com>
2021-11-15 23:37:25 +0800

    label 组件

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,14 +1,34 @@
 <template>
- <layout>
-   <p>Labels.vue</p>
- </layout>
+  <layout>
+    <ol>
+      <li>
+        <span>衣</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right"/>
+      </li>
+    </ol>
+    <div>
+      <button>新建标签</button>
+    </div>
+  </layout>
 </template>
 
 <script lang="ts">
 
 export default {
-  name: "Labels",
-}
+  name: 'Labels',
+};
 </script>
 
 <style lang="scss" scoped>
