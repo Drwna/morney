562668bb9ab3cd1df3b252ee71c3bf9cb75834d2
draganet <3417237276@qq.com>
2022-01-04 21:16:47 +0800

    Labels.vue 使用 Vuex, 抽离 TagHelper 作为 mixin

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button @click="create">新增标签</button>
+      <button @click="createTag">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in tagList" :key="tag.id"
@@ -14,9 +14,9 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-import store from '@/store';
+import {mixins} from 'vue-class-component';
+import TagHelper from '@/mixins/TagHelper';
 
 @Component({
   computed: {
@@ -25,7 +25,7 @@ import store from '@/store';
     }
   }
 })
-export default class Tags extends Vue {
+export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
 
   created() {
@@ -40,13 +40,6 @@ export default class Tags extends Vue {
       this.selectedTags.push(tag);
     }
     this.$emit('update:value', this.selectedTags);
-  }
-
-  create(): void {
-    const name = window.prompt('请输入标签名');
-    if (name === null) return;
-    if (!name.trim()) return window.alert('标签名不能为空！');
-    this.$store.commit('createTag', name);
   }
 }
 </script>
